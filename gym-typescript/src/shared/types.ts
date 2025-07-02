8e644d0dc681e2a9ef3a6730b2a0373b5ac08b62
Raymond Lukwago <lukwagoraymond3@gmail.com>
2025-07-02 22:32:20 +0300

    react-standards-example

---
+++ gym-typescript/src/shared/types.ts
@@ -0,0 +1,18 @@
+export enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus"
+}
+
+export interface BenefitType {
+  icon: React.ReactElement;
+  title: string;
+  description: string;
+}
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
